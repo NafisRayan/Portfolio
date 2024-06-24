f8a18ba70d4f209ed53fb547fc1dc1e7bc6f45e5
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2024-06-25 05:50:21 +0600

    all in all

---
+++ README.md
@@ -0,0 +1,70 @@
+<h2 align="center">
+  Portfolio Website<br/>
+  <!-- Visit the live site: [nafisrayan.tech](https://nafisrayan.tech) -->
+</h2>
+<div align="center">
+  <img alt="Demo" src="./Images/readme-img1.png" />
+</div>
+
+<br/>
+
+<h3 align="center">
+    🔹
+    <a href="https://github.com/NafisRayan/Portfolio/issues">Report Bug</a> &nbsp; &nbsp;
+    🔹
+    <a href="https://github.com/NafisRayan/Portfolio/issues">Request Feature</a>
+</h3>
+
+## TL;DR
+
+You can fork this repo to modify and make changes of your own. Please give me proper credit by linking back to [NafisRayan](https://github.com/NafisRayan/Portfolio). Thanks!
+
+## Built With
+
+My personal portfolio <a href="https://soumyajit.vercel.app/" target="_blank">soumyajit.tech</a> which features some of my github projects as well as my resume and technical skills.<br/>
+
+This project was built using these technologies.
+
+- React.js
+- Node.js
+- Express.js
+- CSS3
+- VsCode
+- Vercel
+
+## Features
+
+**📖 Multi-Page Layout**
+
+**🎨 Styled with React-Bootstrap and Css with easy to customize colors**
+
+**📱 Fully Responsive**
+
+## Attributes
+
+- **Responsive Design**: The website is designed to be responsive, ensuring it looks great on all devices.
+- **Navigation Bar**: A sticky navigation bar for easy site navigation.
+- **Projects Section**: Displays a list of my projects with links to live demos and source codes.
+- **About Me**: A section dedicated to providing more details about myself and my professional background.
+- **Resume**: An embedded resume showcasing my educational qualifications and work experience.
+- **Contact Information**: Easy-to-find contact information for reaching out.
+
+
+## Getting Started
+
+Clone down this repository. You will need `node.js` and `git` installed globally on your machine.
+
+## 🛠 Installation and Setup Instructions
+
+1. Installation: `npm install`
+
+2. In the project directory, you can run: `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The page will reload if you make edits.
+
+## Usage Instructions
+
+Open the project folder and Navigate to `/src/components/`. <br/>
+You will find all the components used and you can edit your information accordingly.
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import ytDownloader from "../../Assets/Projects/ytDownloader.png";
+import yoloEYE from "../../Assets/Projects/yoloEYE.png";
+import chatCom from "../../Assets/Projects/chatCom.png";
+import aiPlayground from "../../Assets/Projects/aiPlayground.png";
+import handControl from "../../Assets/Projects/handControl.png";
+import reactBrowser from "../../Assets/Projects/reactBrowser.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={aiPlayground}
+              isBlog={false}
+              title="AI Playground: Unleash Your Creative Spark!"
+              description="Embark on an AI-driven journey of creativity and exploration with this captivating Streamlit application. Enter your prompts and let the power of Generative AI weave stories, compose poems, generate code snippets, and more. Delve into the realm of imagination, where AI serves as your co-creator, transforming your ideas into tangible expressions."
+              ghLink="https://github.com/NafisRayan/AI-Playground"
+              demoLink="https://ai-playground-5wwl.onrender.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={reactBrowser}
+              isBlog={false}
+              title="ReactNative-Browser"
+              description="This React Native mobile browser application offers users a customized browsing experience on their mobile devices. Key features include web content display using the WebView component, URL navigation (both direct entry and search queries), navigation controls (back, forward, reload, stop), bookmark management, browsing history tracking, profile customization, font size adjustment, and pinch-to-zoom functionality. The app leverages React Native, Expo, React Native Vector Icons, and React Native WebView."
+              ghLink="https://github.com/NafisRayan/ReactNative-Browser"
+              demoLink="https://github.com/NafisRayan/ReactNative-Browser"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatCom}
+              isBlog={false}
+              title="Chat Application: A Flask and Socket.IO-Based Chat Platform"
+              description="This open-source repository houses the code for a versatile chat application developed using Flask and Socket.IO. Key features include user registration, login, participation in a global chat room, private messaging, and message history viewing. The app provides a seamless communication experience for users."
+              ghLink="https://github.com/NafisRayan/Editor.io"
+              demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ytDownloader}
+              isBlog={false}
+              title="YouTube Downloader"
+              description="This project is a simple yet powerful tool designed to download videos from YouTube in either MP4 or MP3 formats. It leverages the Flask framework for its web interface and uses the PyTube library to handle the downloading process."
+              ghLink="https://github.com/NafisRayan/YT-Downloader"
+              demoLink="https://github.com/NafisRayan/YT-Downloader"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={handControl}
+              isBlog={false}
+              title="ThreeJS Hand Control Panel"
+              description="The project is designed to create an interactive 3D visualization of hand landmarks detected in real-time from a video stream, using the MediaPipe Hands model and THREE.js for 3D rendering. It dynamically updates the positions of 3D objects representing hand landmarks based on the detected landmarks, adjusts the camera's position to follow the hand movements, and includes logic to recognize specific hand gestures. The visualization is rendered in a web environment, allowing users to see the hand landmarks in 3D space as they move their hands in front of the camera."
+              ghLink="https://github.com/NafisRayan/ThreeJS-Hand-Control-Panel"
+              demoLink="https://nafisrayan.github.io/ThreeJS-Hand-Control-Panel/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={yoloEYE}
+              isBlog={false}
+              title="yoloEYE: Real-time Object Detection with Streamlit and YOLO Models"
+              description="This project utilizes YOLO (You Only Look Once) models for object detection tasks. It provides a user-friendly interface built with Streamlit, allowing users to easily upload images or video streams to see object detections in real-time. The application supports various YOLO models, including YOLOv8, YOLOv9, and YOLOv10; offering flexibility and accuracy in detecting objects across different scenarios."
+              ghLink="https://github.com/NafisRayan/yoloEYE"
+              demoLink="https://huggingface.co/spaces/vaugheu/yoloEYE"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
